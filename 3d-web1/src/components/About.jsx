0a9721a907dfb369129d080b1952172734e79e08
Abdul-Rahman Alamoudi <abdulrahman_alamoudi@hotmail.com>
2023-03-06 18:13:07 +0300

    almost finish

--- 3d-web1/src/components/About.jsx
+++ 3d-web1/src/components/About.jsx
@@ -1,9 +1,70 @@
-import React from 'react'
+import React from "react";
+import Tilt from "react-tilt";
+import { motion } from "framer-motion";
+
+import { styles } from "../styles";
+import { services } from "../constants";
+import { SectionWrapper } from "../hoc";
+import { fadeIn, textVariant } from "../utils/motion";
+
+const ServiceCard = ({ index, title, icon }) => (
+  <Tilt className="xs:w-[250px] w-full">
+    <motion.div
+      variants={fadeIn("right", "spring", index * 0.5, 0.75)}
+      className="w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card"
+    >
+      <div
+        options={{
+          max: 45,
+          scale: 1,
+          speed: 450,
+        }}
+        className="bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col"
+      >
+        <img
+          src={icon}
+          alt="web-development"
+          className="w-16 h-16 object-contain"
+        />
+
+        <h3 className="text-white text-[20px] font-bold text-center">
+          {title}
+        </h3>
+      </div>
+    </motion.div>
+  </Tilt>
+);
 
 const About = () => {
   return (
-    <div>About</div>
-  )
-}
+    <>
+      <div className="">
+        <motion.div variants={textVariant()}>
+          <p className={styles.sectionSubText}>Introduction</p>
+          <h2 className={styles.sectionHeadText}>Overview.</h2>
+        </motion.div>
 
-export default About
+        <motion.p
+          variants={fadeIn("", "", 0.1, 1)}
+          className="mt-4 text-secondary text-[17px] text-center leading-[30px]"
+          //  max-w-3xl
+        >
+          Web and application Developer specializing in front-end and back- end
+          development. Experienced with all stages of the development cycle for
+          dynamic web and application projects. Well-versed in numerous
+          programming languages including Java, C, C++, Python, HTML5,
+          JavaScript, CSS, SQL. Strong background in project management and
+          customer relations
+        </motion.p>
+
+        <div className="mt-20 flex flex-wrap gap-10 justify-center">
+          {services.map((service, index) => (
+            <ServiceCard key={service.title} index={index} {...service} />
+          ))}
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default SectionWrapper(About, "about");
